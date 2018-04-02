b065a93c43e5253d8abe5b9abd12ad493a560752
Michael Stewart <mistewart@linkedin.com>
2018-04-02 16:38:07 -0700

    setup test and lint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": [
+    "recommended/esnext",
+    "recommended/esnext/style-guide",
+    "recommended/node",
+    "recommended/node/style-guide",
+  ],
+  "rules": {
+    // always require curly braces for if, else, etc.
+    "curly": ["error", "all"],
+    // indent with 2 spaces
+    "indent": ["error", 2],
+    // no spacing in string templates
+    "template-curly-spacing": ["error", "never"],
+    // allow require and module.exports
+    "import/no-commonjs": 0,
+    // use 'self' for capturing 'this' execution context
+    "consistent-this": ["error", "self"],
+  },
+}
